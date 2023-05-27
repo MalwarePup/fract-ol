b22b430258a45f23a44cf5a044be8f2bb9b62bea
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2023-05-27 02:30:02 +0200

    refactor(julia.c): optimize julia function by reducing the number of multiplications and additions (algebraic simplification)

--- src/sets_of_fractal/julia.c
+++ src/sets_of_fractal/julia.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 19:30:24 by ladloff           #+#    #+#             */
-/*   Updated: 2023/05/26 23:29:23 by ladloff          ###   ########.fr       */
+/*   Updated: 2023/05/27 02:29:12 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 
 void	julia(t_complex *z, t_complex c, uint32_t *iter, uint32_t max_iter)
 {
-	double	temp;
+	double		temp_real;
+	double		real_squared;
+	double		imaginary_squared;
 
-	while ((z->real * z->real + z->imaginary * z->imaginary < 4)
-		&& (*iter < max_iter))
+	real_squared = z->real * z->real;
+	imaginary_squared = z->imaginary * z->imaginary;
+	while ((real_squared + imaginary_squared <= 4) && (*iter < max_iter))
 	{
-		temp = z->real * z->real - z->imaginary * z->imaginary + c.real;
+		temp_real = real_squared - imaginary_squared + c.real;
 		z->imaginary = 2 * z->real * z->imaginary + c.imaginary;
-		z->real = temp;
+		z->real = temp_real;
+		real_squared = z->real * z->real;
+		imaginary_squared = z->imaginary * z->imaginary;
 		(*iter)++;
 	}
 }
