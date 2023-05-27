77e89881a00a5496bebd0adb8810298ca6a5e654
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2023-05-27 02:30:14 +0200

    refactor(mandelbrot.c): optimize mandelbrot function by reducing the number of multiplications and additions (algebraic simplification)

--- src/sets_of_fractal/mandelbrot.c
+++ src/sets_of_fractal/mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 13:55:45 by ladloff           #+#    #+#             */
-/*   Updated: 2023/05/26 23:29:38 by ladloff          ###   ########.fr       */
+/*   Updated: 2023/05/27 02:27:46 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,19 @@
 void	mandelbrot(t_complex c, uint32_t *iter, uint32_t max_iter)
 {
 	t_complex	z;
-	double		temp;
+	double		real_squared;
+	double		imaginary_squared;
 
-	z.real = 0;
-	z.imaginary = 0;
-	while ((z.real * z.real + z.imaginary * z.imaginary < 4)
-		&& (*iter < max_iter))
+	z.real = 0.0;
+	z.imaginary = 0.0;
+	real_squared = 0.0;
+	imaginary_squared = 0.0;
+	while ((real_squared + imaginary_squared <= 4) && (*iter < max_iter))
 	{
-		temp = z.real * z.real - z.imaginary * z.imaginary + c.real;
-		z.imaginary = 2 * z.real * z.imaginary + c.imaginary;
-		z.real = temp;
+		z.imaginary = 2.0 * z.real * z.imaginary + c.imaginary;
+		z.real = real_squared - imaginary_squared + c.real;
+		real_squared = z.real * z.real;
+		imaginary_squared = z.imaginary * z.imaginary;
 		(*iter)++;
 	}
 }
