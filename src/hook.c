b9753938cf11da58e69adfe59bebaa3c8015faf5
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2023-05-27 17:19:23 +0200

    refactor(hook.c): extract zoom function to improve code readability and maintainability feat(hook.c): add t_complex parameter to zoom function to allow zooming to a specific point in the fractal fix(hook.c): remove unused variable zoom_factor from hook_mouse_scroll function

--- src/hook.c
+++ src/hook.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 13:43:20 by ladloff           #+#    #+#             */
-/*   Updated: 2023/05/27 14:45:46 by ladloff          ###   ########.fr       */
+/*   Updated: 2023/05/27 16:49:20 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,27 +41,34 @@ int	hook_keypress(int keycode, t_mlx *mlx)
 	return (EXIT_SUCCESS);
 }
 
+void	zoom(t_mlx *mlx, int button, t_complex mouse)
+{
+	double	zoom_factor;
+
+	zoom_factor = 0.0;
+	if (button == SCROLL_UP)
+		zoom_factor = SCROLL_SENSITIVITY;
+	else if (button == SCROLL_DOWN)
+		zoom_factor = 1 / SCROLL_SENSITIVITY;
+	mlx->data.x_min = mouse.real
+		+ (mlx->data.x_min - mouse.real) * zoom_factor;
+	mlx->data.y_min = mouse.imaginary
+		+ (mlx->data.y_min - mouse.imaginary) * zoom_factor;
+	mlx->data.x_max = mouse.real
+		+ (mlx->data.x_max - mouse.real) * zoom_factor;
+	mlx->data.y_max = mouse.imaginary
+		+ (mlx->data.y_max - mouse.imaginary) * zoom_factor;
+	render_fractal(mlx);
+}
+
 int	hook_mouse_scroll(int button, int x, int y, t_mlx *mlx)
 {
 	t_complex	mouse;
-	double		zoom_factor;
 
+	mouse.real = 0.0;
+	mouse.imaginary = 0.0;
 	map_pixel_to_complex(&mouse, x, y, mlx);
 	if (button == SCROLL_UP || button == SCROLL_DOWN)
-	{
-		if (button == SCROLL_UP)
-			zoom_factor = SCROLL_SENSITIVITY;
-		else
-			zoom_factor = 1 / SCROLL_SENSITIVITY;
-		mlx->data.x_min = mouse.real
-			+ (mlx->data.x_min - mouse.real) * zoom_factor;
-		mlx->data.y_min = mouse.imaginary
-			+ (mlx->data.y_min - mouse.imaginary) * zoom_factor;
-		mlx->data.x_max = mouse.real
-			+ (mlx->data.x_max - mouse.real) * zoom_factor;
-		mlx->data.y_max = mouse.imaginary
-			+ (mlx->data.y_max - mouse.imaginary) * zoom_factor;
-		render_fractal(mlx);
-	}
-	return (0);
+		zoom(mlx, button, mouse);
+	return (EXIT_SUCCESS);
 }
