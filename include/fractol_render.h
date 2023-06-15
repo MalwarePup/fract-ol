96a5b0b034509ae43f319be0056fffd32049b852
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2023-06-15 17:22:42 +0200

    chore(fractol_render.h): add documentation for map_pixel_to_complex function

--- include/fractol_render.h
+++ include/fractol_render.h
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 01:00:51 by ladloff           #+#    #+#             */
-/*   Updated: 2023/05/29 01:06:15 by ladloff          ###   ########.fr       */
+/*   Updated: 2023/06/15 17:21:55 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,24 @@
 # include "fractol.h"
 
 /* render.c */
+
+/**
+ * Maps pixel coordinates to complex number coordinates.
+ *
+ * This function takes the pixel coordinates (x, y) of an image and maps them
+ * to corresponding complex number coordinates (real, imaginary) within a
+ * specified range. The range is defined by the t_mlx structure passed as an
+ * argument, which contains the minimum and maximum values for the x and y axes.
+ *
+ * @param c     A pointer to a t_complex structure to store the mapped complex
+ *              number coordinates.
+ * @param x     The x-coordinate of the pixel.
+ * @param y     The y-coordinate of the pixel.
+ * @param mlx   A pointer to a t_mlx structure containing the x and y axis
+ *              ranges.
+ */
 void	map_pixel_to_complex(t_complex *c, int x, int y, t_mlx *mlx);
+
 void	render_fractal(t_mlx *mlx);
 
 #endif /* FRACTOL_RENDER_H */
