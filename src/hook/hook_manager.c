be0fc229c79614235102b212567181286b5b76c7
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2023-06-15 17:24:47 +0200

    style(hook_manager.c): remove unnecessary whitespace and align parameters in mlx_hook call for better readability

--- src/hook/hook_manager.c
+++ src/hook/hook_manager.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 23:22:04 by ladloff           #+#    #+#             */
-/*   Updated: 2023/06/15 13:17:00 by ladloff          ###   ########.fr       */
+/*   Updated: 2023/06/15 16:20:33 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@
 void	hook_manager(t_mlx *mlx)
 {
 	render_fractal(mlx);
-	mlx_hook(mlx->win, DESTROY_NOTIFY, (1L << 17),
-		close_window_event, mlx);
+	mlx_hook(mlx->win, DESTROY_NOTIFY, (1L << 17), close_window_event, mlx);
 	mlx_mouse_hook(mlx->win, hook_mouse_scroll, mlx);
 	mlx_key_hook(mlx->win, hook_keypress, mlx);
 }
