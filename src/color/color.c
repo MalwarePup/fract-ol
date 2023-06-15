3e25126259654836b6c10af724485b335acfefda
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2023-06-15 13:05:08 +0200

    refactor(color.c): change frequency variable type from float to float literal to improve readability fix(color.c): fix floating point precision by changing frequency value to 0.1F

--- src/color/color.c
+++ src/color/color.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 00:22:48 by ladloff           #+#    #+#             */
-/*   Updated: 2023/06/14 18:23:24 by ladloff          ###   ########.fr       */
+/*   Updated: 2023/06/15 13:03:52 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ uint32_t	get_color(int iter, uint32_t color_seed)
 	uint8_t		b;
 	float		frequency;
 
-	frequency = 0.1;
+	frequency = 0.1F;
 	r = (sin(frequency * iter + 4 + sin(color_seed / 30.0)) * 127 + 128);
 	g = (sin(frequency * iter + 2 + cos(color_seed / 20.0)) * 127 + 128);
 	b = (sin(frequency * iter + 1 + tan(color_seed / 10.0)) * 127 + 128);
